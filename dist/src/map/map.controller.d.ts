07ed8608464d53c8240198b81ace13ab23508353
Marquis <abahmarquis@gmail.com>
2025-05-01 07:44:59 +0100

    updatd code

--- dist/src/map/map.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { MapService } from './map.service';
-export declare class MapController {
-    private readonly mapService;
-    constructor(mapService: MapService);
-    getNearbyHospitals(latitude: number, longitude: number, radius: number): Promise<any[]>;
-    getEmergencySurgePoints(): Promise<any[]>;
-    getHospitalDensityHeatmap(): Promise<any[]>;
-}
